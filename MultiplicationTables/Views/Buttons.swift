ef7446f4062d5722f996bdf3a52c36c66359b0cf
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-28 02:01:16 -0700

    add stepper for selecting multiplication table

--- MultiplicationTables/Views/Buttons.swift
+++ MultiplicationTables/Views/Buttons.swift
@@ -36,11 +36,57 @@ struct ChoiceButtons: View {
     }
 }
 
+struct StepperButtons: View {
+    @State var stepDown: Int
+    @State var selectedNum: Int
+    @State var stepUp: Int
+    @State private var hideButtonOne    = false
+    @State private var hideButtonTwo    = false
+    
+    var body: some View {
+        HStack(spacing: 30) {
+            
+            Button(hideButtonOne ? " " : "\(stepDown)"){
+                guard !hideButtonOne else { return }
+                applyStepper(with: -1)
+                }
+            
+                Text("\(selectedNum)")
+                    .font(.largeTitle)
+            Button(hideButtonTwo ? "    " : "\(stepUp)"){
+                guard !hideButtonTwo else { return }
+                applyStepper(with: 1)
+            }
+            
+        }
+        .font(.title)
+        .padding(30)
+        
+    }
+    
+    
+    
+    func applyStepper(with number: Int){
+        stepUp += number
+        selectedNum += number
+        stepDown += number
+        
+        if selectedNum == 2 {
+            hideButtonOne = true
+        }else if selectedNum == 11{
+            hideButtonTwo = true
+        }else{
+            hideButtonOne = false
+            hideButtonTwo = false
+        }
+    }
+}
 
 struct Buttons_Preview: PreviewProvider {
     static var previews: some View {
-//        ProgressButtons(message: "NEXT ->", color: .green)
+        //        ProgressButtons(message: "NEXT ->", color: .green)
         VStack(spacing: 30){
+            StepperButtons(stepDown: 4, selectedNum: 5, stepUp: 6)
             ChoiceButtons(message: "4")
             ChoiceButtons(message: "6")
             ChoiceButtons(message: "19")
