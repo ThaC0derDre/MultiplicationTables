b46280806db13de922048ce15de050f742308d7c
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-09 14:08:50 -0700

    QuizView: display animation when user choice incorrect

--- MultiplicationTables/QuizView.swift
+++ MultiplicationTables/QuizView.swift
@@ -15,33 +15,31 @@ struct QuizView: View {
     @State private var score            = 0
     @State private var flashRed         = false
     var body: some View {
-        
-        Text(quiz[questionNumber].question)
-            .font(.largeTitle.bold())
-        
-        
-        ForEach(0..<3) { number in
-            let choice = choiceGen(with: quiz[questionNumber].choices, for: number)
-            Button("\(choice)"){
-                checkAnswer(userAnswer: choice)
-                
+        VStack{
+            Text(quiz[questionNumber].question)
+                .font(.largeTitle.bold())
+            
+            
+            ForEach(0..<3) { number in
+                let choice = choiceGen(with: quiz[questionNumber].choices, for: number)
+                Button("\(choice)"){
+                    checkAnswer(userAnswer: choice)
+                }
+                .font(.title.bold())
+                .frame(width: 200, height: 75)
+                .background(.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+                .padding()
             }
-            .font(.title.bold())
-            .frame(width: 200, height: 75)
-            .background(.blue)
-            .foregroundColor(.white)
-            .clipShape(Capsule())
-            .padding()
+            .navigationTitle("\(timesTable)'s!")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            
         }
-        
-        
-        
-        .navigationTitle("\(timesTable)'s!")
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarBackButtonHidden(true)
-        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(flashRed ? .red : .white).ignoresSafeArea()
     }
-        
     
     // somehow grab choices into array, shuffled. When called, grab specific
     func choiceGen(with allChoices: Choices, for buttonNumber: Int) -> Int {
@@ -54,18 +52,23 @@ struct QuizView: View {
     func checkAnswer(userAnswer: Int){
         if userAnswer == quiz[questionNumber].choices.correctAnswer{
             score += 1
-            print("correct")
+            questionNumber += 1
         }else{
-            
-            print("incorrect!")
+            withAnimation {
+                flashRed = true
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+                withAnimation {
+                    flashRed = false
+                }
+                questionNumber += 1
+            }
         }
-        questionNumber += 1
     }
-    
 }
 
 
-    
+
 struct QuizView_Previews: PreviewProvider {
     static var previews: some View {
         QuizView(timesTable: 3, qAmount: 3, quiz: [Quiz(question: "What is 2x2?", choices: Choices(choice1: 1, choice2: 2, choice3: 3, correctAnswer: 3))])
