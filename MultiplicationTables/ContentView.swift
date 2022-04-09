54233f33dd3504d3e24edb361147180fc4da78d6
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-09 11:20:01 -0700

    edit main screen ui

--- MultiplicationTables/ContentView.swift
+++ MultiplicationTables/ContentView.swift
@@ -24,124 +24,129 @@ struct ContentView: View {
     @State var quiz = [Quiz]()
     var body: some View {
         
-        NavigationView{
-            // Menu Setup
-            VStack{
-                //MARK: - TimesTable Stepper
-                
-                Text("Choose Times Table")
-                    .font(.largeTitle.bold())
-                    .padding(.bottom)
-                HStack{
-                    // Cute Stepper
-                    Button{
-                        if timesTable == 2 {return}
-                        hidePlus    = false
-                        hideMinus   = false
-                        timesTable -= 1
-                        plusOne -= 1
-                        minusOne -= 1
-                        if timesTable == 2 {
-                            hideMinus = true
-                        }
-                        
-                    } label: {
-                        ZStack{
-                            Image(systemName: "arrowtriangle.left.fill")
-                                .resizable()
-                                .frame(width: 63, height: 60)
-                            
-                                .foregroundColor(hideMinus ? .white : .blue)
-                                .padding(.horizontal, 7)
-                            
-                            Text("  \(minusOne)")
-                                .font(.title.bold())
-                                .foregroundColor(.white)
-                        }
-                    }
-                    // selected number
-                    Text("\(timesTable)")
-                        .font(.largeTitle.bold())
-                        .padding()
-                    
-                    Button{
-                        if timesTable == 12 {return}
-                        
-                        hidePlus    = false
-                        hideMinus   = false
-                        timesTable += 1
-                        minusOne += 1
-                        plusOne += 1
-                        if timesTable == 12 {
-                            hidePlus = true
-                        }
-                    } label: {
-                        ZStack{
-                            Image(systemName: "arrowtriangle.right.fill")
-                                .resizable()
-                                .frame(width: 63, height: 60)
-                                .foregroundColor(hidePlus ? .white : .blue)
-                                .padding(.horizontal, 7)
-                            
-                            Text("  \(plusOne)    .")
-                                .font(.title.bold())
-                                .foregroundColor(.white)
-                        }
+        
+        // Menu Setup
+        VStack{
+            //MARK: - TimesTable Stepper
+            
+            
+            Text("Choose Times Table")
+                .font(.largeTitle.bold())
+                .padding(.bottom)
+                .padding(.top, 50)
+            HStack{
+                // Cute Stepper
+                Button{
+                    if timesTable == 2 {return}
+                    hidePlus    = false
+                    hideMinus   = false
+                    timesTable -= 1
+                    plusOne -= 1
+                    minusOne -= 1
+                    if timesTable == 2 {
+                        hideMinus = true
                     }
                     
-                }
-                
-                //MARK: - Example
-                
-                Text("Ex. \(timesTable) X 1")
-                    .font(.caption)
-                    .foregroundColor(.secondary)
-                    .padding(.bottom)
-                
-                //MARK: - Question Amount
-                
-                // How Many Questions?
-                Text("How Many Questions?")
-                    .font(.largeTitle.bold())
-                
-                HStack{
-                    ForEach(qAmounts, id: \.self){ number in
-                        Button("\(number)"){
-                            withAnimation{
-                                    isSelected = number
-                                questionAmount = number
-                            }
-                        }
-                        .font(.title.bold())
-                        .frame(width: 70, height: 70)
-                        .background(isSelected == number ? .blue : .white)
-                        .foregroundColor(isSelected == number ? .white : .blue)
-                        .overlay(
-                        RoundedRectangle(cornerRadius: 7)
-                            .stroke(.blue, lineWidth: 3)
-                        )
-                        .padding(.horizontal, 10)
+                } label: {
+                    ZStack{
+                        Image(systemName: "arrowtriangle.left.fill")
+                            .resizable()
+                            .frame(width: 63, height: 60)
+                        
+                            .foregroundColor(hideMinus ? .white : .blue)
+                            .padding(.horizontal, 7)
+                        
+                        Text("  \(minusOne)")
+                            .font(.title.bold())
+                            .foregroundColor(.white)
                     }
                 }
+                // selected number
+                Text("\(timesTable)")
+                    .font(.largeTitle.bold())
+                    .padding()
                 
-                
-                
-                Spacer()
-                Button("Let's Go! 💪🏼"){
-                    generateQuestions(questionsAmount: questionAmount, timesTable: timesTable)
-                    showQuiz.toggle()
-                }
-                .frame(width: 200, height: 50)
-                .background(.blue)
-                .foregroundColor(Color(.white))
-                .clipShape(Capsule())
-                .padding(.bottom, 40)
-                
-                NavigationLink("", isActive: $showQuiz){
-                    QuizView(timesTable: timesTable, qAmount: questionAmount, quiz: quiz)
+                Button{
+                    if timesTable == 12 {return}
+                    
+                    hidePlus    = false
+                    hideMinus   = false
+                    timesTable += 1
+                    minusOne += 1
+                    plusOne += 1
+                    if timesTable == 12 {
+                        hidePlus = true
+                    }
+                } label: {
+                    ZStack{
+                        Image(systemName: "arrowtriangle.right.fill")
+                            .resizable()
+                            .frame(width: 63, height: 60)
+                            .foregroundColor(hidePlus ? .white : .blue)
+                            .padding(.horizontal, 7)
+                        
+                        Text("  \(plusOne)    .")
+                            .font(.title.bold())
+                            .foregroundColor(.white)
+                    }
                 }
                 
             }
+            
+            //MARK: - Example
+            
+            Text("Ex. \(timesTable) X 1")
+                .font(.caption)
+                .foregroundColor(.secondary)
+                .padding(.bottom)
+            Spacer()
+            //MARK: - Question Amount
+            
+            // How Many Questions?
+            Text("How Many Questions?")
+                .font(.largeTitle.bold())
+            
+            HStack{
+                ForEach(qAmounts, id: \.self){ number in
+                    Button("\(number)"){
+                        withAnimation{
+                            isSelected = number
+                            questionAmount = number
+                        }
+                    }
+                    
+                    .font(.title.bold())
+                    .frame(width: 70, height: 70)
+                    .background(isSelected == number ? .blue : .white)
+                    .foregroundColor(isSelected == number ? .white : .blue)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 7)
+                            .stroke(.blue, lineWidth: 3)
+                    )
+                    .padding(.horizontal, 10)
+                }
+            }
+            
+            
+            
+            Spacer()
+            Button("Let's Go! 💪🏼"){
+                generateQuestions(questionsAmount: questionAmount, timesTable: timesTable)
+                showQuiz.toggle()
+            }
+            .font(.title)
+            .frame(width: 200, height: 65)
+            .background(.blue)
+            .foregroundColor(Color(.white))
+            .clipShape(Capsule())
+            .padding(.bottom, 40)
+            
+            NavigationLink("", isActive: $showQuiz){
+                QuizView(timesTable: timesTable, qAmount: questionAmount, quiz: quiz)
+            }
+            .navigationTitle("Multiply!")
+            .navigationBarTitleDisplayMode(.inline)
+            
         }
     }
     // Enter amount of Questions asked for, and TimesTable
